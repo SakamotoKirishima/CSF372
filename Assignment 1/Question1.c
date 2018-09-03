2497d696c658d195c0919eb117474b40caa63cbe
SakamotoKirishima <38245044+SakamotoKirishima@users.noreply.github.com>
2018-09-03 17:29:58 +0530

    Started a process

--- Assignment 1/Question1.c
+++ Assignment 1/Question1.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include<unistd.h>
+#include<sys/wait.h>
+int start_process(char **tokens){
+	pid_t process_id= fork();
+	pid_t wpid;
+	int status=0;
+	if (process_id==0) {
+		signal(SIGINT, SIG_IGN);
+		//printf("%s\n", "Child process");
+		//int check= execvp(tokens[0], tokens);
+		//if (check==-1) perror("Error in the child process\n");
+
+	}
+	else if (process_id<0) perror("Error forking\n");
+	else{
+		signal(SIGINT, SIG_IGN);
+		while ((wpid= wait(&status))>0);
+		//printf("%s\n", "Parent process");
+	} 
+	return 1;
+}
 void get_token_error(char **tokens){
 	if (!tokens){
 		fprintf(stderr, "%s\n", "Get tokens error");
@@ -57,19 +78,21 @@ char *read_line(){
 	return realloc(line, sizeof(char)*len);
 }
 int main(){
+	int cont;
 	printf("%s", ">>>" );
 	char *readline;
 	char **tokens=NULL;
 	readline= read_line();
 	//printf("%s\n", readline);
 	tokens= get_tokens(readline);
-	char *token;
+	/*char *token;
 	int i=0;
 	token= tokens[i++];
 	while (token != NULL){
 		printf("%s\n",token );
 		token= tokens[i++];
-	}
+	}*/
+	start_process(tokens);
 	free(readline);
 	free(tokens);
 	return 0;
